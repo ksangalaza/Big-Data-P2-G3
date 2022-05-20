9d082ffa8efc64a5def46d6cdcc3ab3d1f674c13
kolbylingerfelt <103053839+kolbylingerfelt@users.noreply.github.com>
2022-05-20 11:20:34 -0400

    Initial commit for Kolby's Generator
    Customer ID, Customer Name, Order ID

---
+++ KolbyGenerator.scala
@@ -0,0 +1,44 @@
+import scala.util.Random
+import java.io.File
+import java.util.Scanner
+import scala.collection.mutable.ArrayBuffer
+
+object KolbyGenerator {
+    
+    def getOrderId(): Int = {
+        val OrderId = new scala.util.Random
+        var getOrderId = OrderId.nextInt((1000 - 0) + 1)
+        return getOrderId
+    }
+
+    def getCustomerId(): Int ={
+        val CustomerId = new scala.util.Random
+        var getCustomerId = CustomerId.nextInt((20000 - 10001) + 1)
+        return getCustomerId
+    }
+
+    def getCustomerName(): String = {
+        var customersArray = new ArrayBuffer[String]()
+        val file = new File("CustomerNames.txt")
+        var sc = new Scanner(file)
+        var CustomerName = new scala.util.Random
+        var getCustomerName = CustomerName.nextInt((9999 - 0) +1)
+        while(sc.hasNext()){
+            var line = sc.nextLine()
+            if(line == " "){
+
+            }else{
+                customersArray.append(line)
+            }
+        }
+        return customersArray(getCustomerName)
+        
+    }
+
+   
+     def main(args: Array[String]): Unit = {
+         println("Order ID: " + getOrderId())
+         println("Customer ID: " + getCustomerId())
+         println("Customer Name: " + getCustomerName())
+    }
+}
---
+++ MainFile.scala
@@ -0,0 +1,18 @@
+///Necessary imports for CSVGenerator
+import java.io.{BufferedWriter, FileWriter}
+import scala.collection.JavaConversions._
+import scala.collection.mutable.ListBuffer
+import scala.util.Random
+import au.com.bytecode.opencsv.CSVWriter
+
+
+
+
+object MainFile extends App{
+    def main(args: Array[String]): Unit = {
+       ///This is where we will create our main function.
+
+      
+
+    }
+}
